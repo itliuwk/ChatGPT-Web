f813f9329401e101b938e4d5ec76ddc9c1bc051c
刘伟坤 <1834183435@qq.com>
2023-05-30 09:25:27 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/26169320?v=4',
+      name: 'liuwk',
+      description: '<a href="https://github.com/itliuwk" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
